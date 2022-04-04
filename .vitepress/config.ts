aa8c2557286a897504b6a22d46b3618106aecc64
Jaemin Choi <1dotolee@gmail.com>
2022-04-04 07:25:59 +0000

    chore: add favicon

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "dotoleeoak's home",
   description: '민트초코를 싫어하는 minchoi',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }]
+  ],
+  // <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
   vite: {
     server: {
       hmr: {
