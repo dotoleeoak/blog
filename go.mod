64061f6e53c183eb24f801e62667c434732fdbb6
Jaemin Choi <1dotolee@gmail.com>
2024-03-31 03:17:16 -0700

    feat: use hugo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dotoleeoak/blog
+
+go 1.22.1
+
+require github.com/adityatelange/hugo-PaperMod v0.0.0-20240316195851-dad94ab4b7c5 // indirect
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./.vitepress/**/*.{html,ts,vue}'],
-  theme: {
-    extend: {}
-  },
-  plugins: [require('@tailwindcss/typography')]
-}
